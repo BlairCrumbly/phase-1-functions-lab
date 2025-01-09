4e38a0daebf434cf13549228aab013a25003c5b8
Blair Crumbly <blaircrumbly@gmail.com>
2025-01-09 04:49:17 -0500

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks(startingStreetNumber){
+    return Math.abs(startingStreetNumber - 42)
+}
+
+
+function distanceFromHqInFeet(startingStreetNumber){
+    const blocks = distanceFromHqInBlocks(startingStreetNumber); 
+    return blocks * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);  
+  
+    
+    if (distance <= 400) {
+      return 0;
+    } 
+    
+    
+    else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;  
+    } 
+    
+    
+    else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } 
+    
+    
+    else {
+      return 'cannot travel that far';
+    }
+  }
+  
